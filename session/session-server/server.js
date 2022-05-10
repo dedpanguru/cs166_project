ac64bcd9e26f179ca2e911b7d85ee7992c1ab057
Gurv33r <gurveersingh477@gmail.com>
2022-05-10 11:27:16 -0700

    generic session api

--- session/session-server/server.js
+++ session/session-server/server.js
@@ -1,25 +1,49 @@
-import express from "express"
-import session from "express-session"
-import {connect, store} from './database/connect.js'
+const express = require("express")
+const session = require("express-session")
+const cookieParser = require('cookie-parser')
+const MongoDBStore = require("connect-mongodb-session")(session)
+const registerHandler = require("./routes/register.js")
+const loginHandler = require('./routes/login.js')
+const logoutHandler = require('./routes/logout.js')
 
 const app = express()
 
-connect()
+const store= new MongoDBStore({
+    uri: process.env.DB_URL,
+    collection: 'sessions',
+    expires: 1000 * 60 * 60 * 24 * 7,
+    connectionOptions: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 10000
+    }
+})
+
+app.use(express.json({extended:false}))
+
+app.use(cookieParser())
 
 app.use(session({
     secret: process.env.SECRET_KEY || 'secret'.repeat(3),
-    saveUninitialized: falsee,
+    saveUninitialized: true,
     resave: false,
-    store:store,
+    key: 'sid',
+    cookie:{
+        httpOnly: true,
+        secure: true,
+        maxAge:10*60*1000 
+    }
 }))
 
 const port = process.env.SERVER_PORT || 8080
 
 app.get('/', (req, res)=>{
-    console.log(req.session, req.session.id)
-    req.session.isAuth = true
     res.send("Hello World")
 })
+
+app.post('/api/register', registerHandler)
+app.post('/api/login', loginHandler)
+app.post('/api/logout',logoutHandler)
 
 app.listen(port, ()=>{
     console.log(`Server listening on http://localhost:${port}`)
